d0da99aa9d08a232e10e6b5649df60ed188b02a6
Jano <jankois595@gmail.com>
2020-11-19 12:37:15 +0100

    Add project files.

---
+++ JoJo JRPG/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JoJo_JRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
